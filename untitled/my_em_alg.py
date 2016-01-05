99dc2e2173266549842d2ac76620fc5d42fdb743
357589873 <357589873@qq.com>
2016-01-05 10:13:56 +0800

    第一次，包含别人的一个em的算法和自己的几行代码

---
+++ untitled/my_em_alg.py
@@ -0,0 +1,12 @@
+#coding:gbk
+
+import math
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+def run(sigma,):
+
+if __name__ == '__main__':
+    run()
+    plt.hist(X[0,:],20)
+    plt.show()
