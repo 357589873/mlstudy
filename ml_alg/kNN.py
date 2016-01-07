34c7641f9a61d31d929d9d13d99153c6c5bcf9f0
357589873 <357589873@qq.com>
2016-01-07 15:26:08 +0800

    knn结束了，手写识别的那个写完了

--- ml_alg/kNN.py
+++ ml_alg/kNN.py
@@ -1,11 +1,12 @@
 from numpy import *
 import  operator
+from os import listdir
 
 def createDataSet():
     group=array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels=['A','A','B','B']
     return  group,labels
-
+#main coding of classify,select top k of similarity
 def classify0(inX,dataSet,labels,k):
     dataSetsize=dataSet.shape[0]
     diffMat=tile(inX,(dataSetsize,1))-dataSet
@@ -54,10 +55,56 @@ def datingClassTest():
     numTestVecs=int(m*hoRatio)
     for i in range(numTestVecs):
         classifierResult=classify0(normMat[i,:],normMat[numTestVecs:m,:],\
-                                   datingLabels[numTestVecs:m],3)
+                                   datingLabels[numTestVecs:m],10)
         print "the classifier came back with:%d,the real answer is :%d" %(classifierResult,datingLabels[i])
         if (classifierResult!=datingLabels[i]):errorCount+=1.0
 
     print "the total errorrate is %f" % (errorCount/float(numTestVecs))
 
+def classifyPerson():
+    resultList=['not at all','in small doses','in large doses']
+    percentTats=float(raw_input("percentage of time spent playing video games?"))
+    ffMiles=float(raw_input("frequent filer miles earned per year"))
+    iceCream=float(raw_input("liters of ice cream consumed per year?"))
+    datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals=autoNorm(datingDataMat);
+    inArr=array([ffMiles,percentTats,iceCream])
+    classifierResult=classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print "you probably like this person :",resultList[classifierResult-1]
 
+
+#handwriting recognization
+#file is 32*32 but we create a array of 1*1024 to store these chars
+def img2vector(filename):
+    returnVect=zeros((1,1024))
+    fr=open(filename)
+    for i in range(32):
+        lineBtr=fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j]=int(lineBtr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels=[]
+    trainingFileList=listdir('trainingDigits')
+    m=len(trainingFileList)
+    trainingMat=zeros((m,1024))
+    for i in range(m):
+        fileNameStr=trainingFileList[i]
+        fileStr=fileNameStr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:]=img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList=listdir('testDigits')
+    errorCount=0.0
+    mTest=len(testFileList)
+    for i in range(mTest):
+        fileNameStr=testFileList[i]
+        fileStr=fileNameStr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])
+        vectorUnderTest=img2vector('testDigits/%s' % fileNameStr)
+        classifierResult=classify0(vectorUnderTest,trainingMat,hwLabels,5)
+        print "the classifier came back with:%d,the real answer is %d" % (classifierResult,classNumStr)
+        if(classifierResult !=classNumStr):errorCount+=1.0
+    print "\nthe total number of errors is:%d" % errorCount
+    print "\nthe total error rate is: %f "% (errorCount/float(mTest))
