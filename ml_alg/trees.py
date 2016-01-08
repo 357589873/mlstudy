0340e7c01f5400bb14bcdd21a5074e76965689cb
357589873 <357589873@qq.com>
2016-01-08 18:26:03 +0800

    现在已经生产树了，但是还没有matplotlib的标签

--- ml_alg/trees.py
+++ ml_alg/trees.py
@@ -1,4 +1,7 @@
+#coding: UTF-8
 from math import log
+
+import operator
 
 def calcShannnEnt(dataSet):
     numEntries=len(dataSet)
@@ -27,3 +30,51 @@ def splitDataSet(dataSet,axis,value):
             reducedFeatVec.extend(featVec[axis+1:])
             retDataSet.append(reducedFeatVec)
     return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures=len(dataSet[0])-1
+    baseEntropy=calcShannnEnt(dataSet)
+    bestInfoGain=0.0;bestFeature=-1
+    for i in range(numFeatures):
+        featList=[example[i] for example in dataSet]
+        uniqueVals=set(featList)
+        newEntropy=0.0
+        for value in uniqueVals:
+            subDataSet=splitDataSet(dataSet,i,value)
+            prob=len(subDataSet)/float(len(dataSet))
+            newEntropy+=prob*calcShannnEnt(subDataSet)
+        infoGain=baseEntropy-newEntropy
+        if(infoGain>bestInfoGain):
+            bestInfoGain=infoGain
+            bestFeature=i
+    return bestFeature
+#
+def majorityCnt(classList):
+    classCount={}
+    for vote in classList:
+        if vote not in classCount.keys():classCount[vote]=0
+        classCount[vote]+=1
+    sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+    classList=[example[-1] for example in dataSet]
+    #如果标签里面所有值都一样，直接返回
+    if classList.count(classList[0])==len(classList):
+        return classList[0]
+    #如果特征只剩一个，那么就调用此函数开始多数表决来决定哪个分类
+    if len(dataSet[0])==1:
+        return majorityCnt(classList)
+    bestFeat=chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel=labels[bestFeat]
+    myTree={bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues=[example[bestFeat] for example in dataSet]
+    uniqueVals=set(featValues)
+    for value in uniqueVals:
+        subLabels=labels[:]
+        myTree[bestFeatLabel][value]=createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+    return myTree
+
+
+
