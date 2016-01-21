1ac860f82d3fcc9c5cd830b83a6cd68b1a0df4a1
357589873 <357589873@qq.com>
2016-01-21 19:21:48 +0800

    logistic画图结束了。接下来就是批梯度下降

--- ml_alg/logRegres.py
+++ ml_alg/logRegres.py
@@ -24,3 +24,30 @@ def gradAscent(dataMatIn,classLabels):
         error=(labelMat-h)
         weights=weights+alpha*dataMatrix.transpose()*error
     return weights
+
+def plotBestFit(wei):
+    import matplotlib.pyplot as plt
+    weights=array(wei)
+    #print type(wei)
+    #print type(weights)
+    dataMat,labelMat=loadDataSet()
+    dataArr=array(dataMat)
+    n=shape(dataMat)[0]
+    xcord1=[];ycord1=[]
+    xcord2=[];ycord2=[]
+    for i in range(n):
+        if int(labelMat[i])==1:
+            xcord1.append(dataArr[i,1]);ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]);ycord2.append(dataArr[i,2])
+    print xcord1,ycord1
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    #s是代表圈的大小，c是颜色，maeker是代表形状。s就是正方形的意思
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+    x=arange(-3.0,3.0,0.1)
+    y=(-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x,y)
+    plt.xlabel('X1');plt.ylabel('X2');
+    plt.show()
