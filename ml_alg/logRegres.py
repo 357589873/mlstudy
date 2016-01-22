65c063fca8d62aefbf609ff742364ebc3c387372
357589873 <357589873@qq.com>
2016-01-22 17:25:08 +0800

    logistic回归写完了

--- ml_alg/logRegres.py
+++ ml_alg/logRegres.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from math import  *
 from numpy import *
 def loadDataSet():
@@ -51,3 +52,73 @@ def plotBestFit(wei):
     ax.plot(x,y)
     plt.xlabel('X1');plt.ylabel('X2');
     plt.show()
+
+def stocGradAscent0(dataMatrix,classLabels):
+    m,n=shape(dataMatrix)
+    alpha=0.01
+    weights=ones(n)
+    for i in range(m):
+        h=sigmod(sum(dataMatrix[i]*weights))
+        error=classLabels[i]-h
+        weights=weights+alpha*error*dataMatrix[i]
+    return weights
+
+def stocGradAscent1(dataMatrix,classLabels,numIter=150):
+    m,n=shape(dataMatrix)
+    weights=ones(n)
+    for j in range(numIter):
+        dataIndex=range(m)
+        for i in range(m):
+            alpha=4/(1.0+j+i)+0.01
+            randIndex=int(random.uniform(0,len(dataIndex)))
+            h=sigmod(sum(dataMatrix[randIndex]*weights))
+            error=classLabels[randIndex]-h
+            weights=weights+alpha*error*dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+def classifyVector(inX,weights):
+    prob=sigmod(sum(inX*weights))
+    if prob>0.5:return  1.0
+    else:return 0.0
+
+def colicTest():
+    frTrain=open('horseColicTraining.txt')
+    frTest=open('horseColicTest.txt')
+    trainingSet=[];trainingLabels=[]
+    for line in frTrain.readlines():
+        currLine=line.strip().split('\t')
+        lineArr=[]
+        #print  line
+        #print  currLine[0:21]
+        #lineArr.append(float(currLine[:,21]))
+        #构造数据集
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        #print lineArr
+        trainingSet.append(lineArr)
+        #构造类别集
+        trainingLabels.append(currLine[21])
+    #print trainingSet
+    #求参数
+    trainWeights=stocGradAscent1(array(trainingSet),trainingLabels,500)
+    errorCount=0;numTestVec=0.0
+    for line in frTest.readlines():
+        numTestVec+=1.0
+        currLine=line.strip().split('\t')
+        lineArr=[]
+        #构造测试数据集
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        #求结果
+        if int(classifyVector(array(lineArr),trainWeights))!=int(currLine[21]):
+            errorCount+=1
+    errorRate=(float(errorCount)/numTestVec)
+    print 'the error rate of this test is :%f ' % errorRate
+    return errorRate
+
+def multiTest():
+    numTests=10;errorSum=0.0
+    for k in range(numTests):
+        errorSum+=colicTest()
+    print "after %d iteration the average error rate is :%f" % (numTests,errorSum/float(numTests))
