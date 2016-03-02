f71e14fe7d02fc8c96ea77279484186203b9b827
357589873 <357589873@qq.com>
2016-03-02 18:27:26 +0800

    加权类型的回归，通过判断公式里的k值，来控制欠拟合和过拟合。并能画图表示。

--- ml_alg/regression.py
+++ ml_alg/regression.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from numpy import *
 def loadDataset(fileName):
     numFeat=len(open(fileName).readline().split('\t'))-1
@@ -23,11 +24,61 @@ def standRegres(xArr,yArr):
 
 def thisDraw(xArr,yArr,ws):
     xMat=mat(xArr)
+    print xMat
+    print xArr
+    print array(xArr)
     yMat=mat(yArr)
     yHat=xMat*ws
     import matplotlib.pyplot as plt
     fig=plt.figure()
     ax=fig.add_subplot(111)
+    #我就是看看这里到底是干什么，原来用了mat后必须这么做，要是arr就好了
+    print array(xArr)[:,1]
+    print xMat[:,1]
+    print xMat[:,1].flatten()
+    print xMat[:,1].flatten().A[0]
     ax.scatter(xMat[:,1].flatten().A[0],yMat.T[:,0]
                .flatten().A[0])
+    xCopy=xMat.copy()
+    xCopy.sort(0)
+    yHat=xCopy*ws
+    print xCopy[:,1],yHat
+    #plot这个函数比较好，就算加了.flatten().A[0]变成arr一样可以。
+    ax.plot(xCopy[:,1],yHat)
+    plt.show()
 
+#局部加权线性回归函数
+#中心思想是来了数据再进行计算，不是事先求好回归方程
+def lwlr(testPoint,xArr,yArr,k=1.0):
+    xMat=mat(xArr);yMat=mat(yArr).T
+    m=shape(xMat)[0]
+    weights=mat(eye((m)))
+    for j in range(m):
+        diffMat=testPoint-xMat[j,:]
+        weights[j,j]=exp(diffMat*diffMat.T/(-2.0*k**2))
+    xTx=xMat.T*(weights*xMat)
+    if linalg.det(xTx)==0.0:
+        print 'this matrix is singular,change'
+        return
+    ws=xTx.I*(xMat.T*(weights*yMat))
+    return testPoint*ws
+
+def lwlrTest(testArr,xArr,yArr,k=1.0):
+    m=shape(testArr)[0]
+    yHat=zeros(m)
+    for i in range(m):
+        yHat[i]=lwlr(testArr[i],xArr,yArr,k)
+    return yHat
+#通过调节k的值来确定拟合的程度，0.3的时候就直线了。0.001过拟合
+def thisDraw2(xArr,yArr,k=0.001):
+    yHat=lwlrTest(xArr,xArr,yArr,k)
+    xMat=mat(xArr)
+    srtInd=xMat[:,1].argsort(0)
+    xSort=xMat[srtInd][:,0,:]
+    import matplotlib.pyplot as plt
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    ax.plot(xSort[:,1],yHat[srtInd])
+    ax.scatter(xMat[:,1].flatten().A[0],mat(yArr).T.flatten()
+               .A[0],s=2,c='red')
+    plt.show()
