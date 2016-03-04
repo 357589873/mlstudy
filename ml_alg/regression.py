efd0f77ce40ee3fee351f78edf455e60577f5ea6
357589873 <357589873@qq.com>
2016-03-04 18:06:05 +0800

    加入了前向逐步线性回归

--- ml_alg/regression.py
+++ ml_alg/regression.py
@@ -83,6 +83,10 @@ def thisDraw2(xArr,yArr,k=0.001):
                .A[0],s=2,c='red')
     plt.show()
 
+def rssError(yArr,yHatArr):
+    return ((yArr-yHatArr)**2).sum()
+
+#岭回归
 def ridgeRegres(xMat,yMat,lam=0.2):
     xTx=xMat.T*xMat
     denom=xTx+eye(shape(xMat)[1])*lam
@@ -90,7 +94,9 @@ def ridgeRegres(xMat,yMat,lam=0.2):
         print "this matrix is singular,can not inverse"
         return
     ws=denom.I*(xMat.T*yMat)
-    return
+    #print ws
+    #ws是一个1*m的矩阵
+    return ws
 
 def ridgeTest(xArr,yArr):
     xMat=mat(xArr);yMat=mat(yArr).T
@@ -104,4 +110,35 @@ def ridgeTest(xArr,yArr):
     for i in range(numTestPts):
         ws=ridgeRegres(xMat,yMat,exp(i-10))
         wMat[i,:]=ws.T
+        break
     return wMat
+#數據標準化
+def regularize(xMat):
+    xMeans=mean(xMat,0)
+    xVar=var(xMat,0)
+    xMat=(xMat-xMeans)/xVar
+    return xMat
+#前向逐步线性回归
+def stageWise(xArr,yArr,eps=0.01,numIt=100):
+    xMat=mat(xArr);yMat=mat(yArr).T
+    yMean=mean(yMat,0)
+    yMat=yMat-yMean
+    xMat=regularize(xMat)
+    m,n=shape(xMat)
+    returnMat=zeros((numIt,n))
+    ws=zeros((n,1));wsTest=ws.copy();wsMax=ws.copy()
+    for i in range(numIt):
+        print ws.T
+        lowestError=inf
+        for j in range(n):
+            for sign in [-1,1]:
+                wsTest=ws.copy()
+                wsTest[j]+=eps*sign
+                yTest=xMat*wsTest
+                rssE=rssError(yMat.A,yTest.A)
+                if rssE<lowestError:
+                    lowestError=rssE
+                    wsMax=wsTest
+        ws=wsMax.copy()
+        returnMat[i,:]=ws.T
+    return  returnMat
