00d2b2af506b0dba8f76aeb91e38ec3622973a00
357589873 <357589873@qq.com>
2016-03-07 18:32:46 +0800

    乐高、谷歌api

--- ml_alg/regression.py
+++ ml_alg/regression.py
@@ -142,3 +142,77 @@ def stageWise(xArr,yArr,eps=0.01,numIt=100):
         ws=wsMax.copy()
         returnMat[i,:]=ws.T
     return  returnMat
+
+#预测乐高的价格
+from time import sleep
+import  json
+import  urllib2
+def searchForTest(retX,retY,setNum,yr,numPce,origPrc):
+    sleep(10)
+    myAPIstr='AIzaSyD2cR2KFyx12hXu6PFU-wrWot3NXvko8vY'
+    searchURL='https://www.googleapis.com/shopping/search/v1/public/' \
+              'products?key=%s&country=US&q=lego+%d%alt=json' % (myAPIstr,setNum)
+    pg=urllib2.urlopen(searchURL)
+    retDict=json.loads(pg.read())
+    for i in range(len(retDict['items'])):
+        try:
+            currItem=retDict['items'][i]
+            if currItem['product']['condition']=='new':
+                newFlag=1
+            else:newFlag=0
+            listOfInv=currItem['product']['inventories']
+            for item in listOfInv:
+                sellingPrice=item['price']
+                if sellingPrice>origPrc*0.5:
+                    print "%d\t%d\t%d\t%f\t%f" %\
+                          (yr,numPce,newFlag,origPrc,sellingPrice)
+                    retX.append([yr,numPce,newFlag,origPrc,sellingPrice])
+                    retY.append(sellingPrice)
+        except:print 'problem with item %d' % i
+
+def setDataCollect(retX,retY):
+    searchForTest(retX,retY,8288,2006,800,49.99)
+    searchForTest(retX,retY,10030,2002,3096,269.99)
+    searchForTest(retX,retY,10179,2007,5195,499.99)
+    searchForTest(retX,retY,10181,2007,3428,199.99)
+    searchForTest(retX,retY,10189,2008,5922,299.99)
+    searchForTest(retX,retY,10196,2009,3263,249.99)
+
+def scrapePage(inFile,outFile,yr,numPce,origPrc):
+    from BeautifulSoup import BeautifulSoup
+    fr = open(inFile); fw=open(outFile,'a') #a is append mode writing
+    soup = BeautifulSoup(fr.read())
+    i=1
+    currentRow = soup.findAll('table', r="%d" % i)
+    while(len(currentRow)!=0):
+        currentRow = soup.findAll('table', r="%d" % i)
+        title = currentRow[0].findAll('a')[1].text
+        lwrTitle = title.lower()
+        if (lwrTitle.find('new') > -1) or (lwrTitle.find('nisb') > -1):
+            newFlag = 1.0
+        else:
+            newFlag = 0.0
+        soldUnicde = currentRow[0].findAll('td')[3].findAll('span')
+        if len(soldUnicde)==0:
+            print "item #%d did not sell" % i
+        else:
+            soldPrice = currentRow[0].findAll('td')[4]
+            priceStr = soldPrice.text
+            priceStr = priceStr.replace('$','') #strips out $
+            priceStr = priceStr.replace(',','') #strips out ,
+            if len(soldPrice)>1:
+                priceStr = priceStr.replace('Free shipping', '') #strips out Free Shipping
+            print "%s\t%d\t%s" % (priceStr,newFlag,title)
+            fw.write("%d\t%d\t%d\t%f\t%s\n" % (yr,numPce,newFlag,origPrc,priceStr))
+        i += 1
+        currentRow = soup.findAll('table', r="%d" % i)
+    fw.close()
+
+def setDataCollect():
+    scrapePage('setHtml/lego8288.html','out.txt', 2006, 800, 49.99)
+    scrapePage('setHtml/lego10030.html','out.txt', 2002, 3096, 269.99)
+    scrapePage('setHtml/lego10179.html','out.txt', 2007, 5195, 499.99)
+    scrapePage('setHtml/lego10181.html','out.txt', 2007, 3428, 199.99)
+    scrapePage('setHtml/lego10189.html','out.txt', 2008, 5922, 299.99)
+    scrapePage('setHtml/lego10196.html','out.txt', 2009, 3263, 249.99)
+
