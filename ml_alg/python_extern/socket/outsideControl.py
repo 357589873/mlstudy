5a144482a8c9e20705addc2165caa9c678f89623
357589873 <357589873@qq.com>
2016-03-10 18:55:02 +0800

    第一次

---
+++ ml_alg/python_extern/socket/outsideControl.py
@@ -0,0 +1,47 @@
+#coding:utf-8
+#第三方发送控制命令到服务器。
+import socket
+import sys
+import  binascii
+import struct
+import urllib2, urllib
+HOST='182.92.79.42'
+PORT=7777
+numberErr=0
+
+BUFFER=1024
+test_str_on='64 00 00 00 00 00 00 00 00 00 00 00 00 00 1a fe 34 f7 54 e1 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01'
+def strconvert(s):
+    s=str(s).strip().split(' ')
+    my=r'\x'
+    fin=''
+    for i in range(len(s)):
+        fin=fin+struct.pack('B',int(s[i],16))
+    return fin
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+import time
+while(1):
+    try :
+        #Set the whole string
+        sock.sendto(strconvert(test_str_on),(HOST,PORT))
+    except socket.error,e:
+        #Send failed
+        print 'Send failed'
+        print e,sys.exc_info()[0],sys.exc_info()[1]
+        #sys.exit()
+    try:
+        print "recvfrom"
+        data,addr= sock.recvfrom(BUFFER)
+        print "afterecv"
+        print int(binascii.b2a_hex(data[8]),16),'月',int(binascii.b2a_hex(data[9]),16),"日 星期",int(binascii.b2a_hex(data[10]),16),\
+            int(binascii.b2a_hex(data[11]),16),'点',int(binascii.b2a_hex(data[12]),16)
+        numberErr=0
+    except socket.error,e:
+        print "timedelay....numnerErr =" ,numberErr
+        numberErr+=1
+
+        if numberErr==10:
+            sys.exit()
+    time.sleep(3)
+    print "again"
+sock.close()
