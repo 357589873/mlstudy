cdfb5e5471f194b6d6ed44ab07951c77b8d90528
357589873 <357589873@qq.com>
2016-03-28 18:35:43 +0800

    kMeans.py finish,but still some bugs.

--- ml_alg/kMeans.py
+++ ml_alg/kMeans.py
@@ -14,8 +14,11 @@ def distEclud(vecA,vecB):
 
 def randCent(dataSet,k):
     n=shape(dataSet)[1]
+    #print "dataSet is "
+    #print len(dataSet),dataSet
     centroids=mat(zeros((k,n)))
     for j in range(n):
+        #print j,dataSet[:,j]
         minJ=min(dataSet[:,j])
         #print minJ
         rangeJ=float(max(dataSet[:,j])-minJ)
@@ -27,7 +30,7 @@ def kMeans(dataSet,k,distMeans=distEclud,createCent=randCent):
     m=shape(dataSet)[0]
     clusterAssment=mat(zeros((m,2)))
     centroids=createCent(dataSet,k)
-    print centroids
+    #print centroids
     clusterChanged=True
     while clusterChanged:
         clusterChanged=False
@@ -40,15 +43,15 @@ def kMeans(dataSet,k,distMeans=distEclud,createCent=randCent):
             #看一下clusterAss这个数组里存的变化了没有
             if clusterAssment[i,0]!=minIndex:clusterChanged=True
             clusterAssment[i,:]=minIndex,minDist**2
-        print clusterAssment
+        #print clusterAssment
         for cent in range(k):
-            print cent,nonzero(clusterAssment[:,0].A==cent)
+            #print cent,nonzero(clusterAssment[:,0].A==cent)
             #nonzeros函数是求出来的二维，取[0]可以只要x维的。y维不需要
             ptsInClust=dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]
             print ptsInClust
             #axis代表两个两个方向。0代表竖向
             centroids[cent,:]=mean(ptsInClust,axis=0)
-            print centroids
+            #print centroids
     return centroids,clusterAssment
 
 #二分均值聚类
@@ -64,12 +67,14 @@ def biKmeans(dataSet,k,distMeans=distEclud):
         for i in range(len(centList)):
             ptsInCurrCluster=dataSet[nonzero(clusterAssment[:,0].A==i)\
                 [0],:]
+            print "ptsInCurrCluster is"
+            print ptsInCurrCluster
             centroidMat,splitClustAss=kMeans(ptsInCurrCluster,2,distMeans)
             sseSplit=sum(splitClustAss[:,1])
             #未划分的簇的SSE的值
             sseNotSplit=\
             sum(clusterAssment[nonzero(clusterAssment[:,0].A!=i)[0],1])
-            print 'sseSplit,sseNotSplit:',sseSplit,sseNotSplit
+            #print 'sseSplit,sseNotSplit:',sseSplit,sseNotSplit
             #划分的和没划分的加一起，看看SSE是不是最小的。
             if(sseSplit+sseNotSplit)<lowestSSE:
                 bestCentToSplit=i
@@ -79,8 +84,8 @@ def biKmeans(dataSet,k,distMeans=distEclud):
         #新的两个需要替换原来的索引。一个是替换原来的位置，一个是替换最后加上的位置
         bestClustAss[nonzero(bestClustAss[:,0].A==1)[0],0]=len(centList)
         bestClustAss[nonzero(bestClustAss[:,0].A==0)[0],0]=bestCentToSplit
-        print 'the bestcenttosplit is :',bestCentToSplit
-        print 'the len of bestClustAss is:',len(bestClustAss)
+        #print 'the bestcenttosplit is :',bestCentToSplit
+        #print 'the len of bestClustAss is:',len(bestClustAss)
         #替换原来那个，然后新增一个，这样就成功的二分
         centList[bestCentToSplit]=bestNewCents[0,:]
         centList.append(bestNewCents[1,:])
@@ -101,7 +106,7 @@ def geoGrab(stAddress,city):
     yahooApi=apiStem+url_params
     print yahooApi
     c=urllib.urlopen(yahooApi)
-    return json.loads(c.read)
+    return json.loads(c.read())
 
 from time import sleep
 def massPlaceFind(fileName):
@@ -118,12 +123,41 @@ def massPlaceFind(fileName):
         else:print "error fetching"
         sleep(1)
     fw.close()
-
+#地球表面亮点间的距离distance between two point in earth
 def distSLC(vecA,vecB):
     a=sin(vecA[0,1]*pi/180)*sin(vecB[0,1]*pi/180)
     b=cos(vecA[0,1]*pi/180)*cos(vecB[0,1]*pi/180)*\
       cos(pi*(vecB[0,0]-vecA[0,0])/180)
     return arccos(a+b)*6371.0
+
+import matplotlib
+import  matplotlib.pyplot as plt
+def clusterClubs(numClust=5):
+    datList=[]
+    for line in open('places.txt').readlines():
+        lineArr=line.split('\t')
+        datList.append([float(lineArr[4]),float(lineArr[3])])
+    datMat=mat(datList)
+    #key function clust
+    myCentroids,clustAssing=biKmeans(datMat,numClust,distMeans=distSLC)
+    fig=plt.figure()
+    rect=[0.1,0.1,0.8,0.8]
+    scatterMarkers=['s','o','^','8','p','d','v','h','>','<']
+    axprops=dict(xticks=[],yticks=[])
+    ax0=fig.add_axes(rect,label='ax0',**axprops)
+    imgP=plt.imread('Portland.png')
+    ax0.imshow(imgP)
+    ax1=fig.add_axes(rect,label='ax1',frameon=False)
+    for i in range(numClust):
+        ptsInCurrCluster=datMat[nonzero(clustAssing[:,0].A==i)[0],:]
+        markerStyle=scatterMarkers[i % len(scatterMarkers)]
+        ax1.scatter(ptsInCurrCluster[:,0].flatten().A[0],\
+                    ptsInCurrCluster[:,1].flatten().A[0],\
+                    marker=markerStyle,s=90)
+    ax1.scatter(myCentroids[:,0].flatten().A[0],\
+                myCentroids[:,1].flatten().A[0],marker='+',s=300)
+    plt.show()
+
 
 
 
